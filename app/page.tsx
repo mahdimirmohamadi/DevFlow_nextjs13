90670cfde3950de9f9766e02a126f4937239f576
Mahdi MirMohamadi <mahdimirmohamadi13@gmail.com>
2024-03-01 19:05:23 +0330

    Add Basic Routing system

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Hello friend</h1>
-      <h2 className="h2-bold">Hello friend</h2>
-      <h3 className="h3-bold">Hello friend</h3>
-    </div>
-  );
-}
